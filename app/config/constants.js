12f1e75301bda8dde78b0f5763b7eea10de9f59d
Michail Brynard <mbrynard@gmail.com>
2017-08-17 17:46:29 -0700

    Update service url

--- app/config/constants.js
+++ app/config/constants.js
@@ -2,5 +2,5 @@ export default {
   keyboardAvoidingOffset: 75,
   company_id: "luuun",
   rehive_url: "https://rehive.com/api/3/",
-  stellar_service_url: "https://rehive.com/services/crypto/",
+  stellar_service_url: "https://stellar.services.rehive.io/api/1/",
 }
