fa718d6b6cb1ea97b38892217cf2f1b2b6f927b9
BS-134\BS134 <tarekmahmud@brainstation-23.com>
2017-10-13 21:06:34 +0600

    Contacts issue solved by pagination

--- app/services/contactService.js
+++ app/services/contactService.js
@@ -1,74 +1,77 @@
 import Expo from 'expo'
-import { Alert } from 'react-native'
+import {Alert} from 'react-native'
 
 var contactService = {
-  getAllContacts: async () => {
-    const permission = await Expo.Permissions.askAsync(Expo.Permissions.CONTACTS)
-    if (permission.status !== 'granted') {
-      Alert.alert(
-        'Error',
-        'Permission denied'
-      )
-      return
-    }
-    const getTotal = await Expo.Contacts.getContactsAsync({
-      fields: [
-        Expo.Contacts.PHONE_NUMBERS,
-        Expo.Contacts.EMAILS,
-        Expo.Contacts.THUMBNAIL,
-      ],
-      pageSize: 1,
-      pageOffset: 0,
-    })
+    getAllContacts: async () => {
+        const time = Date.now();
+        const permission = await Expo.Permissions.askAsync(Expo.Permissions.CONTACTS)
+        if (permission.status !== 'granted') {
+            Alert.alert(
+                'Error',
+                'Permission denied'
+            )
+            return
+        }
 
-    const contacts = await Expo.Contacts.getContactsAsync({
-      fields: [
-        Expo.Contacts.PHONE_NUMBERS,
-        Expo.Contacts.EMAILS,
-        Expo.Contacts.THUMBNAIL,
-      ],
-      pageSize: getTotal.total,
-      pageOffset: 0,
-    })
 
-    //console.log(contacts)
-
-    var data = []
-    contacts.data.forEach((node) => {
-      if (typeof (node.phoneNumbers) !== "undefined") {
-        node.phoneNumbers.forEach((number) => {
-          var newData = {
-            name: node.name,
-            contact: number.number,
-          }
-          data.push(newData)
+        const getTotal = await Expo.Contacts.getContactsAsync({
+            fields: [
+                Expo.Contacts.PHONE_NUMBERS,
+                Expo.Contacts.EMAILS,
+                Expo.Contacts.THUMBNAIL,
+            ],
+            pageSize: 60,
+            pageOffset: 0,
         })
-      }
-      if (typeof (node.emails) !== "undefined") {
-        node.emails.forEach((email) => {
-          var newData = {
-            name: node.name,
-            contact: email.email,
-          }
-          data.push(newData)
+
+        const contacts = await Expo.Contacts.getContactsAsync({
+            fields: [
+                Expo.Contacts.PHONE_NUMBERS,
+                Expo.Contacts.EMAILS,
+                Expo.Contacts.THUMBNAIL,
+            ],
+            pageSize: getTotal.total,
+            pageOffset: 0,
         })
-      }
-    })
+        /*const elapsed = (Date.now() - time) / 1000;
+        Alert.alert('Contacts Read', `Read ${contacts.data.length} contacts in ${elapsed} seconds`);*/
 
-    data = data.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1
-      }
-      else if (a.name > b.name) {
-        return 1
-      }
-      else {
-        return 0
-      }
-    })
+        var data = []
+        contacts.data.forEach((node) => {
+            if (typeof (node.phoneNumbers) !== "undefined") {
+                node.phoneNumbers.forEach((number) => {
+                    var newData = {
+                        name: node.name,
+                        contact: number.number,
+                    }
+                    data.push(newData)
+                })
+            }
+            if (typeof (node.emails) !== "undefined") {
+                node.emails.forEach((email) => {
+                    var newData = {
+                        name: node.name,
+                        contact: email.email,
+                    }
+                    data.push(newData)
+                })
+            }
+        })
 
-    return data
-  },
+        data = data.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            else if (a.name > b.name) {
+                return 1
+            }
+            else {
+                return 0
+            }
+        })
+
+        return data
+    },
 }
 
 export default contactService
