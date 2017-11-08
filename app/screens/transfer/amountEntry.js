bf6f3db657e10d06912d713bafe76e7c3bea2492
BS-134\BS134 <tarekmahmud@brainstation-23.com>
2017-11-08 20:22:17 +0600

    use big for float calculation

--- app/screens/transfer/amountEntry.js
+++ app/screens/transfer/amountEntry.js
@@ -6,6 +6,7 @@ import ResetNavigation from './../../util/resetNavigation'
 import TextInput from './../../components/textInput'
 import Colors from './../../config/colors'
 import Header from './../../components/header'
+import Big from 'big.js'
 
 export default class AmountEntry extends Component {
   static navigationOptions = {
@@ -34,11 +35,10 @@ export default class AmountEntry extends Component {
     else {
       const data = await AsyncStorage.getItem('currency')
       const currency = JSON.parse(data)
-      let amount = this.state.amount
+      let amount = new Big(this.state.amount)
       for (let i = 0; i < currency.divisibility; i++) {
-        amount = amount * 10
+        amount = amount.times(10)
       }
-      amount = parseInt(amount, 10)
       Alert.alert(
         'Are you sure?',
         'Send ' + currency.symbol + this.state.amount + ' to ' + this.state.reference,
