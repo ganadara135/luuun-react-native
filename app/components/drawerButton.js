96aee294a79c86bfca912d424559034adf211a23
tsmrafee-bs <tarekmahmud@brainstation-23.com>
2017-12-20 15:25:07 +0600

    touch efficiency add

--- app/components/drawerButton.js
+++ app/components/drawerButton.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import {TouchableOpacity} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import PropTypes from 'prop-types';
 
-const DrawerButton = ({ navigation }) => (
-  <TouchableOpacity style={{padding: 10}} underlayColor={'white'}>
-    <Icon
-      name="ios-menu-outline"
-      size={30}
-      color="white"
-      onPress={() => navigation.navigate('DrawerOpen')}
-    />
-  </TouchableOpacity>
+const DrawerButton = ({navigation}) => (
+    <TouchableOpacity
+        onPress={() => navigation.navigate('DrawerOpen')}
+        style={{padding: 10}} underlayColor={'white'}>
+        <Icon
+            name="ios-menu-outline"
+            size={30}
+            color="white"
+        />
+    </TouchableOpacity>
 )
 
 DrawerButton.propTypes = {
-  navigation: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired,
 };
 
 export default DrawerButton
