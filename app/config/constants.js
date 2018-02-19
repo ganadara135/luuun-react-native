3235fe66e32e12c083b81837efecae7f40d02dc6
tsmrafee-bs <tarekmahmud@brainstation-23.com>
2018-02-19 16:37:53 +0600

    baseurl changed

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,6 +1,6 @@
 export default {
   keyboardAvoidingOffset: 75,
   company_id: "luuun",
-  rehive_url: "https://rehive.com/api/3/",
+  rehive_url: "https://api.rehive.com/3/",
   stellar_service_url: "https://stellar.services.rehive.io/api/1/",
 }
