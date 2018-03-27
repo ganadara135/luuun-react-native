42b12011d7c359cfc281d08b6d41b94dde9daf2b
tsmrafee <tsmrafee@gmail.com>
2018-03-27 19:29:09 +0600

    balance update

--- app/screens/home/transactions.js
+++ app/screens/home/transactions.js
@@ -109,13 +109,13 @@ export default class Transactions extends Component {
     }
 
     handleRefresh() {
-        //this.props.updateBalance()
-        if (this.state.loading !== true) {
-            this.setState({refreshing: true});
-            this.getData(this.state.currency).then(() => {
-                this.setState({refreshing: false});
-            })
-        }
+        this.props.updateBalance()
+        // if (this.state.loading !== true) {
+        //     this.setState({refreshing: true});
+        //     this.getData(this.state.currency).then(() => {
+        //         this.setState({refreshing: false});
+        //     })
+        // }
     }
 
 
